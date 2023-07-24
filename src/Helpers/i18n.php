868997fdc09305286c7498c01d1e9a1b310530a6
Harun Baris Bulut <baris.bulut@plusclouds.com>
2023-07-24 18:11:18 +0300

    Helper added

--- src/Helpers/i18n.php
+++ src/Helpers/i18n.php
@@ -6,6 +6,7 @@ class i18n
 {
     public static function t($text, $toLang = 'en') : string
     {
+        //  Burada gelen text'i alıp hash'leyip, veritabanına kayıt edeceğiz.
         return $text;
     }
 }
