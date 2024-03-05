7e4710124207cbe127de7e0cd75e487ea5a50848
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-03-05 19:58:56 +0300

    Translation is fixed

--- src/Http/api.routes.php
+++ src/Http/api.routes.php
@@ -2,20 +2,22 @@
 
 Route::prefix('i18n')->group(function() {
     Route::prefix('translations')->group(function () {
-            Route::get('/', 'I18nTranslation\I18nTranslationController@index');
-            Route::get('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@show');
-            Route::post('/', 'I18nTranslation\I18nTranslationController@store');
-            Route::put('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@update');
-            Route::delete('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@destroy');
-        });
+        Route::get('/', 'I18nTranslation\I18nTranslationController@index');
+        Route::get('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@show');
+        Route::post('/', 'I18nTranslation\I18nTranslationController@store');
+        Route::put('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@update');
+        Route::delete('/{i18n-translations}', 'I18nTranslation\I18nTranslationController@destroy');
+    });
 
 // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+    Route::prefix('translate')->group(function() {
+        Route::post('/', 'I18nTranslation\TranslationController@store');
+    });
 
     Route::prefix('translation')->group(function() {
         Route::get('/list.vue', 'I18nTranslation\TranslationController@jsVue');
         Route::get('/list.json', 'I18nTranslation\TranslationController@jsJson');
         Route::get('/generate', 'I18nTranslation\TranslationController@generate');
-
-        Route::post('/', 'I18nTranslation\TranslationController@store');
     });
 });
